e9095265e3adb7de05008a049614f0800c20781c
Theinterna <yalvin58@gmail.com>
2024-09-03 22:13:36 -0700

    Set up tensorflow and cocoSsd
    Use useEffect to load model before the rest of the page

--- src/ObjectDetection.jsx
+++ src/ObjectDetection.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import '@tensorflow/tfjs';
+import * as cocoSsd  from "@tensorflow-models/coco-ssd"
 
 const ObjectDetection = () => {
+
+    const [model, setModel] = useState(null);
+    const [imageSrc, setImageSrc] = useState(null);
+
+    useEffect(() => {
+        const loadModel = async () => {
+            const loadedModel = await cocoSsd.load();
+            setModel(loadedModel);
+        };
+        loadModel();
+        // [] = empty dependencies is used so that the model only loads once after initial render
+        // you can technically reload when it is triggered again
+        // but once model is loaded, there should be nothing else to load
+        // hence, we use [].
+    }, [])
+
     return (
         <div>
             <ObjectDetection />
